dffa2ddb8ed74fcad3768c247f2e4ed616f18b01
Strltz <matushin_savva@mail.ru>
2026-01-20 15:51:45 +0300

    add tests, but without 3rd contest

--- contest 3/task2/task2/task2/task2.cpp
+++ contest 3/task2/task2/task2/task2.cpp
@@ -31,76 +31,42 @@ std::vector<std::string> split_space(std::string the_string) {
     return words;
 }
 
-class TheBeatles {
+class FilterBloom {
 public:
     std::vector<int8_t> the_data;
-    size_t size_data_bits;
+    int size_bits_;
+    std::vector<unsigned long long> prime_numbers_for_hash;
+    long long m;
+    long long k;
 
-    TheBeatles() {
+    FilterBloom() {
         the_data = { };
-        size_data_bits = 0;
+        size_bits_ = 0;
+        prime_numbers_for_hash = { 2, 3 };
+        m = 0;
+        k = 0;
     }
 
-    void resize_(size_t new_size_bits) {
-        if (new_size_bits > size_data_bits) {
-            size_data_bits = new_size_bits;
-            the_data.resize(((new_size_bits + 7) / 8), 0);
-        }
+    void new_filter(int m_, int k_) {
+        m = m_;
+        k = k_;
+        size_bits_ = m;
+        resize_(size_bits_);
+        new_prime_numbers(k);
     }
 
-    std::string set_bit(size_t t) {
-        if (t >= size_data_bits) {
-            return "error";
-        }
-        else {
-            the_data[t / 8] = the_data[t / 8] | (1 << (t - (t / 8) * 8));
-        }
-        return "";
-    }
-
-    std::pair<std::string, bool> get_bit(size_t t) {
-        if (t >= the_data.size() * 8) {
-            return { "error", false };
-        }
-        else {
-            bool for_ret = ((the_data[t / 8] & (1 << (t - (t / 8) * 8))) >> (t - (t / 8) * 8) == 1);
-            //bool for_ret = the_data[t / 8] / int(pow(2, t % 8)) % 2 == 1;
-            if (for_ret) {
-                return { "", true };
-            }
-            else {
-                return { "", false };
-            }
-        }
-    }
-
-    void print_(std::ostream& the_out) {
-        for (size_t i = 0; i < size_data_bits; ++i) {
-            the_out << get_bit(i).second;
-        }
-    }
-};
-
-class PrimeNumbers {
-public:
-    std::vector<unsigned long long> prime_numbers;
-
-    PrimeNumbers() {
-        prime_numbers = { 2, 3 };
-    }
-
-    void new_numbers(size_t n_count) {
-        if (n_count > prime_numbers.size()) {
-            for (size_t i = prime_numbers[prime_numbers.size() - 1] + 2; n_count > prime_numbers.size(); i += 2) {
+    void new_prime_numbers(size_t n_count) {
+        if (n_count > prime_numbers_for_hash.size()) {
+            for (size_t i = prime_numbers_for_hash[prime_numbers_for_hash.size() - 1] + 2; n_count > prime_numbers_for_hash.size(); i += 2) {
                 bool prm = true;
-                for (size_t j = 0; j < prime_numbers.size() && j < prime_numbers[j]; ++j) {
-                    if (i % prime_numbers[j] == 0) {
+                for (size_t j = 0; j < prime_numbers_for_hash.size() && prime_numbers_for_hash[j] < (sqrt(i) + 2); ++j) {
+                    if (i % prime_numbers_for_hash[j] == 0) {
                         prm = false;
                         break;
                     }
                 }
                 if (prm) {
-                    prime_numbers.push_back(i);
+                    prime_numbers_for_hash.push_back(i);
                 }
             }
         }
@@ -109,47 +75,62 @@ public:
         }
     }
 
+    void resize_(size_t new_size_bits) {
+        the_data.resize(ceil(new_size_bits / 8), 0);
+        size_bits_ = new_size_bits;
+    }
 
-};
+    bool set_bit(size_t x) {
+        if (x >= size_bits_) {
+            return false;
+        }
+        else {
+            int byte = x / 8;
+            x -= byte * 8;
+            the_data[byte] = the_data[byte] | (1 << x);
+        }
+    }
 
-class FilterBloom {
-public:
-    std::vector<int8_t> the_data;
-    int size_bits_;
-    std::vector<unsigned long long> prime_numbers = { 2, 3 };
-    PrimeNumbers primes_for_hash;
-    long long m;
-    long long k;
+    std::pair<bool, bool> get_bit(size_t x) {
+        if (x >= size_bits_) {
+            return { false, false };
+        }
+        else {
+            int byte = x / 8;
+            x -= byte * 8;
+            if ((the_data[byte] & (1 << x)) >> x == 1) {
+                return { true, true };
+            }
+            else {
+                return{ true, false };
+            }
+        }
+    }
 
-    FilterBloom() {
-        m = 0;
-        k = 0;
+    void print_(std::ostream& the_out) {
+        for (size_t i = 0; i < m; ++i) {
+            the_out << get_bit(i).second;
+        }
     }
 
     unsigned long long hash_func_i(long long i, unsigned long long x) {
-        return (((i + 1) * x + primes_for_hash.prime_numbers[i]) % M_Mersen) % m;
+        return (((i + 1) * x + prime_numbers_for_hash[i]) % M_Mersen) % size_bits_;
     }
 
     void add_(unsigned long long x) {
         for (size_t i = 0; i < k; ++i) {
-            bits_.set_bit(hash_func_i(i, x));
+            set_bit(hash_func_i(i, x));
         }
     }
 
-    std::pair<bool, std::string> search_(unsigned long long x) {
+    bool search_(unsigned long long x) {
         for (size_t i = 0; i < k; ++i) {
-            if (!bits_.get_bit(hash_func_i(i, x)).second) {
-                if (bits_.get_bit(hash_func_i(i, x)).first == "") {
-                    return { false, "" };
-                }
-                else {
-                    return { false, "error" };
-                }
+            if (!get_bit(hash_func_i(i, x)).second) {
+                return false;
             }
         }
-        return { true, "" };
+        return true;
     }
-
 };
 
 
@@ -182,16 +163,15 @@ int main()
                     double P = std::stod(this_line[2]);
 
                     if (P > 0 && P < 1 && n >= 1) {
-                        the_filter.m = round(-n * log2(P) / log(2));
+                        the_filter.m = round(abs(n * log2(P) / log(2)));
                         the_filter.k = round(-log2(P));
 
                         if (the_filter.k < 1) {
                             std::cout << "error" << std::endl;
                         }
                         else {
-                            the_filter.bits_.resize_(abs(the_filter.m));
-                            the_filter.primes_for_hash.new_numbers(the_filter.k);
                             is_set = true;
+                            the_filter.new_filter(the_filter.m, the_filter.k);
                             std::cout << abs(the_filter.m) << " " << the_filter.k << std::endl;
                         }
                     }
@@ -204,21 +184,16 @@ int main()
                 the_filter.add_(std::stoull(this_line[1]));
             }
             else if (this_line[0] == "search" && this_line.size() == 2 && is_set) {
-                std::pair<bool, std::string> ret = the_filter.search_(std::stoull(this_line[1]));
-                if (ret.second == "") {
-                    if (ret.first) {
-                        std::cout << "1" << std::endl;
-                    }
-                    else {
-                        std::cout << "0" << std::endl;
-                    }
+                auto ret = the_filter.search_(std::stoull(this_line[1]));
+                if (ret) {
+                    std::cout << "1" << std::endl;
                 }
                 else {
-                    std::cout << "error" << std::endl;
+                    std::cout << "0" << std::endl;
                 }
             }
             else if (str_enter == "print" && is_set) {
-                the_filter.bits_.print_(std::cout);
+                the_filter.print_(std::cout);
                 std::cout << std::endl;
             }
             else {
